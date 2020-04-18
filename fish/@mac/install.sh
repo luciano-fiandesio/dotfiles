265aca4795095e6ba9be66a1b72644fc80f45665
Luciano Fiandesio <luciano@fiandes.io>
2020-04-18 10:39:50 +0200

    minor fixes

--- fish/@mac/install.sh
+++ fish/@mac/install.sh
@@ -2,7 +2,7 @@
 MYSHELL=`echo $SHELL | sed -e 's/\/.*\///g'`
 # set fish as shell as default 
 if [ "$MYSHELL" != "fish" ]; then
-    echo “/usr/local/bin/fish” | sudo tee -a /etc/shells
+    echo /usr/local/bin/fish | sudo tee -a /etc/shells
     chsh -s /usr/local/bin/fish
     echo "fish is now the default shell :)" 
 fi
