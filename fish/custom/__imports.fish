56d1fa2720de0a700da40346d77e4d40bee210d5
Luciano Fiandesio <luciano@fiandes.io>
2021-03-03 17:37:58 +0100

    GoogleCloud fish (darwin)

--- fish/custom/__imports.fish
+++ fish/custom/__imports.fish
@@ -1,4 +1,9 @@
 # Source Exports
 source ~/.config/fish/custom/_export.fish
+
+source ~/.config/fish/custom/_export.local.fish
 # Source Aliases
 source ~/.config/fish/custom/_alias.fish
+
+# Google cloud
+[ (uname) = 'Darwin' ] && source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
