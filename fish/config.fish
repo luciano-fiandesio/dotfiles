ed502fd3e39c8018c480908dba4dbece84adcae9
Luciano Fiandesio <luciano@fiandes.io>
2021-03-15 12:18:43 +0100

    brew for linux and osx

--- fish/config.fish
+++ fish/config.fish
@@ -7,9 +7,15 @@ set fish_greeting
 # Setting up the Path
 set -e fish_user_paths
 
-#homebrew
-set -U fish_user_paths "/usr/local/sbin" $fish_user_paths
-
+# homebrew
+switch (uname)
+    case Linux
+       fish_add_path /home/linuxbrew/.linuxbrew/bin
+    case Darwin
+       fish_add_path /usr/local/sbin
+    case '*'
+       echo Hi, stranger!
+end
 source ~/.config/fish/custom/__imports.fish
 
 # load custom functions
