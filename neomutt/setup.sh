a4db4c632858137e9a73cbc64b8ce9681659384c
Luciano Fiandesio <luciano@fiandes.io>
2021-07-08 19:44:30 +0200

    OSX/Linux compatible neomutt config

--- neomutt/setup.sh
+++ neomutt/setup.sh
@@ -5,4 +5,18 @@ if ! [ -x "$(command -v stow)" ]; then
   exit 1
 fi
 
-stow -v -d ~/.dotfiles/ -S neomutt -t ~/ --ignore setup.sh
+#stow -v -d ~/.dotfiles/ -S neomutt -t ~/ 
+
+# generate msmtp config file
+TEMPLATE="msmtprc.template"
+case $(uname | tr '[:upper:]' '[:lower:]') in
+      linux*)
+        sed  "/ca-certificates/s/^#//g" $TEMPLATE > $HOME/.msmtprc
+        ;;
+      darwin*)
+        sed  "/cert.pem/s/^#//g" $TEMPLATE > $HOME/.msmtprc
+        ;;
+      *)
+        echo "Hi, stranger!"
+        ;;
+esac
