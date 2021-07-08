21561615445d0b77b15fbd71fd5291bbd51883e0
Luciano Fiandesio <luciano@fiandes.io>
2021-07-08 20:26:50 +0200

    smart mailcap!

--- neomutt/setup.sh
+++ neomutt/setup.sh
@@ -12,9 +12,11 @@ TEMPLATE="msmtprc.template"
 case $(uname | tr '[:upper:]' '[:lower:]') in
       linux*)
         sed  "/ca-certificates/s/^#//g" $TEMPLATE > $HOME/.msmtprc
+        ln -s .config/neomutt/mailcap.linux $HOME/.config/neomutt/mailcap 
         ;;
       darwin*)
         sed  "/cert.pem/s/^#//g" $TEMPLATE > $HOME/.msmtprc
+        ln -s .config/neomutt/mailcap.osx $HOME/.config/neomutt/mailcap 
         ;;
       *)
         echo "Hi, stranger!"
