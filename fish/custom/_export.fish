dcd3992856a02b9e85e63f4df65bbf29e766426a
Luciano Fiandesio <luciano@fiandes.io>
2022-01-18 11:06:56 +0100

    misc

--- fish/custom/_export.fish
+++ fish/custom/_export.fish
@@ -4,9 +4,6 @@ set -x GPG_TTY (tty)
 # EDITOR
 set -Ux EDITOR nvim
 
-# local bin - small scripts
-fish_add_path ~/bin
-
 # GOLANG
 set -x GOPATH ~/go-workspace
 [ (uname) = 'Darwin' ] && set -x GOROOT /usr/local/opt/go/libexec
